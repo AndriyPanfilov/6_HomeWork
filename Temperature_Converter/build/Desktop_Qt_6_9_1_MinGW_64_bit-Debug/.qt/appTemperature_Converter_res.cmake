8d2afea7d2fe32bc8dbdb6694e71b10db0ee945c
Andriy Pan <lexxlexxpan@gmail.com>
2025-06-20 22:56:41 +0300

    Remove build folder from repo

--- Temperature_Converter/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/appTemperature_Converter_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appTemperature_Converter")
-set(working_dir "D:/1_Programming/Hellil IT shcool/Home_Work/6_HomeWork/Temperature_Converter")
-set(src_and_dest_list
-
-)
-set(timestamp_file "D:/1_Programming/Hellil IT shcool/Home_Work/6_HomeWork/Temperature_Converter/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/appTemperature_Converter_res.txt")
--- Temperature_Converter/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/bin/qt_setup_tool_path.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=F:\Qt\6.9.1\mingw_64\bin;%PATH%
-%*
